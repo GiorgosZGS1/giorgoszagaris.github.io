be42612f48fcf3def33d7cb9a5452a9994195769
cywoo1 <54222841+GiorgosZGS1@users.noreply.github.com>
2022-02-07 23:58:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const menu = document.querySelector(".links");
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+menuBtn.onclick = () => {
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+}
+
+closeBtn.onclick = () => {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+}
+
+
+const Texts = [
+    'HTML', 'CSS', 'JAVASCRIPT',
+    'BOOTSTRAP', 'PYTHON','PHOTOSHOP',
+    'MYSQL','WORDPRESS','FIGMA'
+
+];
+
+var tagCloud = TagCloud('.sphere', Texts, {
+    radius: 230,
+    maxSpeed: 'normal',
+    initSpeed: 'fast',
+    direction: 135,
+    keep: true
+});
+
+let color = '#008b8b';
+document.querySelector('.sphere').style.color = color;
